19cfcb76c06e58d7031bc458c9ebac39d62d1939
Richard Hull <rm_hull@yahoo.co.uk>
2014-01-24 00:52:29 +0000

    Rudimentary on-key and on-mouse handling

--- src/big_bang/core.cljs
+++ src/big_bang/core.cljs
@@ -1,12 +1,12 @@
 (ns big-bang.core
   (:require
     [cljs.core.async :refer [<! >! chan]]
-    [big-bang.protocol :refer [data-channel shutdown!]]
+    [big-bang.protocol :refer [data-channel shutdown! no-op]]
     [big-bang.package :refer [package? extract-message extract-world-state]]
     [big-bang.timer :refer [interval-ticker]]
     [big-bang.event-handler :refer [add-event-listener]])
   (:require-macros
-    [cljs.core.async.macros :refer [go]]))
+    [cljs.core.async.macros :refer [go alt!]]))
 
 (def animation-frame
   (or (.-requestAnimationFrame js/window)
@@ -29,26 +29,36 @@
   (let [history-builder (if record? conj (constantly nil))
         limit-reached? (if frame-limit #(> % frame-limit) (constantly false))
         stop-when? (or stop-when? (constantly false))
-        ticker (interval-ticker (or tick-rate 17))] ; 17ms = approx 58.82 FPS
+        ticker (interval-ticker (or tick-rate 17)) ; 17ms = approx 58.82 FPS
+        keydown-handler (if on-key
+                          (add-event-listener (.-body js/document) :keydown)
+                          (no-op))
+        mouse-handler (if on-mouse
+                          (add-event-listener (.-body js/document) :click)
+                          (no-op))]
     (go
       (loop [world-state initial-state
              history     []
              frame       0]
-        (<! (data-channel ticker))
-        (if (or (limit-reached? frame) (stop-when? world-state))
+        (let [handler-result (alt!
+                               (data-channel keydown-handler) ([event ch] (on-key event world-state))
+                               (data-channel mouse-handler)   ([event ch] (on-mouse event world-state))
+                               (data-channel ticker)          ([value ch] (on-tick world-state)))
+              next-world-state (extract-world-state handler-result)
+              message (extract-message handler-result)]
+          (send-message send-channel message)
+          (if (or (limit-reached? frame) (stop-when? next-world-state))
 
-          ; initiate shutdown
-          (do
-            (shutdown! ticker)
-            history)
+            ; initiate shutdown
+            (do
+              (shutdown! ticker)
+              (shutdown! keydown-handler)
+              history)
 
-          ; keep on truckin'
-          (let [handler-result (on-tick world-state)
-                next-world-state (extract-world-state handler-result)
-                message (extract-message handler-result)]
-            (animation-frame (fn [] (to-draw world-state)))
-            (send-message send-channel message)
-            (recur
-              (extract-world-state next-world-state)
-              (history-builder history next-world-state)
-              (inc frame))))))))
+            ; keep on truckin'
+            (do
+              (animation-frame (fn [] (to-draw world-state)))
+              (recur
+                next-world-state
+                (history-builder history next-world-state)
+                (inc frame)))))))))
