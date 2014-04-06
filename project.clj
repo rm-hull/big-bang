8d45aac90527bb2792c3c11a1f07326cd25a1eea
Richard Hull <rm_hull@yahoo.co.uk>
2014-04-06 16:53:51 +0100

    Updated dependency versions

--- project.clj
+++ project.clj
@@ -4,14 +4,14 @@
   :clojurecript? true
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2173"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
                  [rm-hull/cljs-dataview "0.0.1-SNAPSHOT"]
-                 [rm-hull/cljs-test "0.0.7"]
+                 [rm-hull/cljs-test "0.0.8-SNAPSHOT"]
                  [rm-hull/dommy "0.1.3-SNAPSHOT"]
                  [rm-hull/monet "0.1.10"]]
-  :plugins [[lein-cljsbuild "1.0.1"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :source-paths ["src"]
   :cljsbuild {
     :test-commands  {"phantomjs"  ["phantomjs" "target/unit-test.js"]}
